458612f38a7bfd6f2d3699c6926a6fdacd8ae6db
LEE-GYEO-RE <rufp9891@gmail.com>
2025-08-12 09:44:53 +0900

    스프링 8일차 종합8 예제 BoardService 12

--- src/main/java/종합/예제12/controller/BoardController.java
+++ src/main/java/종합/예제12/controller/BoardController.java
@@ -5,19 +5,57 @@ import org.springframework.web.bind.annotation.*;
 import 종합.예제12.model.dto.BoardDto;
 import 종합.예제12.service.BoardService;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
-@RequestMapping("/board") // 공통 URL
+@RequestMapping("/board") // + 공통 URL
 public class BoardController {
-    @Autowired // service 의존성 주입
-    private BoardService boardService;
+    @Autowired private BoardService boardService;
 
-    // 게시물 등록
-    @PostMapping("") // 주소 생략
-    public boolean boardWrite(@RequestBody BoardDto boardDto){
-        System.out.println("boardDto = " + boardDto);
+    // [1] 등록
+    @PostMapping("") // localhost:8080/board
+    public boolean boardWrite(@RequestBody BoardDto boardDto ){
         System.out.println("BoardController.boardWrite");
-        boolean result = boardService.boardWrite(boardDto);
+        System.out.println("boardDto = " + boardDto);
+        boolean result = boardService.boardWrite( boardDto ); // 서비스 호출 하고 응답을 반환
         return result;
-    } // func e
+    }
 
-} // class e
+    // [2] 전체조회
+    @GetMapping("") // localhost:8080/board
+    public List<BoardDto> boardPrint(){
+        System.out.println("BoardController.boardPrint");
+        List<BoardDto> result = boardService.boardPrint();
+        return result;
+    }
+
+    // [3] 개별조회
+    @GetMapping("/find") // localhost:8080/board/find?bno=1
+    public BoardDto boardFind( @RequestParam int bno ){
+        System.out.println("BoardController.boardFind");
+        System.out.println("bno = " + bno);
+        BoardDto result = boardService.boardFind( bno );
+        return result;
+    }
+
+    // [4] 개별삭제
+    @DeleteMapping("") // localhost:8080/board?bno=3
+    public boolean boardDelete( @RequestParam int bno ){
+        System.out.println("BoardController.boardDelete");
+        System.out.println("bno = " + bno);
+        boolean result = boardService.boardDelete( bno );
+        return result;
+    }
+
+    // [5] 개별수정
+    @PutMapping("") // localhost:8080/board
+    public boolean boardUpdate( @RequestBody BoardDto boardDto ){
+        System.out.println("BoardController.boardUpdate");
+        System.out.println("boardDto = " + boardDto);
+        boolean result = boardService.boardUpdate( boardDto );
+        return result;
+    }
+
+
+} // CLASS END
